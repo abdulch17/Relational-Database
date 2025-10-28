b3fdde0af638d5c82d9f17419bfa9466eab56d32
abdulch17 <Abdulch1703@gmail.com>
2025-10-28 15:59:37 +0000

    final script for element

--- period_table/element.sh
+++ period_table/element.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 # Script to get element info
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  # check if input is a number
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ATOMIC_NUMBER=$1
+  else
+    # get atomic number by name or symbol
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name = '$1' OR symbol = '$1';")
+  fi
+
+  # if atomic number still not found
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number = $ATOMIC_NUMBER;")
+    SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $ATOMIC_NUMBER;")
+    TYPE_ID=$($PSQL "SELECT type_id FROM properties WHERE atomic_number = $ATOMIC_NUMBER;")
+    TYPE=$($PSQL "SELECT type FROM types WHERE type_id=$TYPE_ID;")
+    MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number = $ATOMIC_NUMBER;")
+    MELTING=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER;")
+    BOILING=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER;")
+
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+  fi
+fi
